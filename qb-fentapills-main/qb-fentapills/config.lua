fb2cff671166d7f7186f6272a938f06ff4e44bf0
SpyX <94620332+spyx4@users.noreply.github.com>
2025-09-19 20:44:32 +0100

    Add files via upload

---
+++ qb-fentapills-main/qb-fentapills/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+-- Farming positions
+Config.FarmComponentAPos = vector3(-1601.85, 3093.27, 32.57) -- Broken Pills farm
+Config.FarmComponentBPos = vector3(-2422.45, 4257.89, 7.78) -- Fenta Syrup farm
+Config.LabPos            = vector3(1417.95, 6330.21, 25.58) -- Lab (mixing)
+
+-- Props per zone
+Config.FarmProps = {
+    componentA = {`prop_barrel_exp_01a`, `prop_rad_waste_barrel_01`, `prop_barrel_02a`},
+    componentB = {`prop_box_wood05a`, `prop_boxpile_07d`, `prop_box_wood02a`}
+}
